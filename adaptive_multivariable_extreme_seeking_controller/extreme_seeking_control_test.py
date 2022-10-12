bd04e0f96be379bf2b4568bf6d42e7f64a22032e
Jun Zeng <zengjunsjtu@berkeley.edu>
2022-10-11 17:55:25 -0700

    Add formatting with black.

--- adaptive_multivariable_extreme_seeking_controller/extreme_seeking_control_test.py
+++ adaptive_multivariable_extreme_seeking_controller/extreme_seeking_control_test.py
@@ -3,115 +3,144 @@ import matplotlib.pyplot as plt
 
 from extremum_seeking_control import esc
 
+
 def cost_function(speed, sideslip):
-    return 100 + speed**2 + 50 * np.power(sideslip-np.pi/2, 2)
-    
-##################
-dt = 0.02 
-end_time = 500.0
+    return 100 + speed**2 + 50 * np.power(sideslip - np.pi / 2, 2)
 
-init_speed = 5.0 #in m/s
-init_sideslip = 40 * np.pi / 180 #in deg
 
-speed_disturbance_magnitude= 0.5 #[m/s]
-speed_frequency= 1.0 #[rad/s] 
-sideslip_disturbance_magnitude = 7.5 * np.pi /180
-sideslip_frequency = 0.5 #[rad/s]
-integrator_gain = -0.05
-##################
+def run_simulation():
 
-step_num = np.int(end_time/dt)
+    # Simulation Setup
+    dt = 0.02
+    end_time = 500.0
 
-speed_extremum_seeking = esc(dt, speed_frequency, speed_disturbance_magnitude, integrator_gain,
-                                                  init_speed, speed_frequency, speed_frequency, True)
-sideslip_extremum_seeking = esc(dt, sideslip_frequency, sideslip_disturbance_magnitude, integrator_gain,
-                                                  init_sideslip, sideslip_frequency, sideslip_frequency, True)
+    init_speed = 5.0  # in m/s
+    init_sideslip = 40 * np.pi / 180  # in deg
 
-#data to log
-time_log = np.zeros((step_num, 1))
-cost_log = np.zeros((step_num, 1))
+    speed_disturbance_magnitude = 0.5  # [m/s]
+    speed_frequency = 1.0  # [rad/s]
+    sideslip_disturbance_magnitude = 7.5 * np.pi / 180
+    sideslip_frequency = 0.5  # [rad/s]
+    integrator_gain = -0.05
+    step_num = np.int(end_time / dt)
 
-speed_es_output_log = np.zeros((step_num, 1))
-speed_high_pass_output_log = np.zeros((step_num, 1))
-speed_low_pass_output_log = np.zeros((step_num, 1))
-speed_m2_sqrt_log = np.zeros((step_num, 1))
-speed_gradient_log = np.zeros((step_num, 1))
-speed_setpoint_log = np.zeros((step_num, 1))
+    # Construct controller
+    speed_extremum_seeking = esc(
+        dt,
+        speed_frequency,
+        speed_disturbance_magnitude,
+        integrator_gain,
+        init_speed,
+        speed_frequency,
+        speed_frequency,
+        True,
+    )
+    sideslip_extremum_seeking = esc(
+        dt,
+        sideslip_frequency,
+        sideslip_disturbance_magnitude,
+        integrator_gain,
+        init_sideslip,
+        sideslip_frequency,
+        sideslip_frequency,
+        True,
+    )
 
-sideslip_es_output_log = np.zeros((step_num, 1))
-sideslip_high_pass_output_log = np.zeros((step_num, 1))
-sideslip_low_pass_output_log = np.zeros((step_num, 1))
-sideslip_m2_sqrt_log = np.zeros((step_num, 1))
-sideslip_gradient_log = np.zeros((step_num, 1))
-sideslip_setpoint_log = np.zeros((step_num, 1))
+    # Logging data
+    time_log = np.zeros((step_num, 1))
+    cost_log = np.zeros((step_num, 1))
 
-t = 0
-current_speed = init_speed
-current_sideslip = init_sideslip
-cost_init = cost_function(init_speed, init_sideslip)
-for i in range(step_num):
-    cost = cost_function(current_speed, current_sideslip)
-    [speed_es_output, speed_high_pass_output, speed_low_pass_output, 
-        speed_m2_sqrt, speed_gradient, speed_setpoint] = speed_extremum_seeking.update(cost-cost_init)
-    [sideslip_es_output, sideslip_high_pass_output, sideslip_low_pass_output, 
-        sideslip_m2_sqrt, sideslip_gradient, sideslip_setpoint] = sideslip_extremum_seeking.update(cost-cost_init)
-    
-    #log data
-    time_log[i] = t
-    cost_log[i] = cost
-    speed_es_output_log[i] = current_speed
-    speed_high_pass_output_log[i] = speed_high_pass_output
-    speed_low_pass_output_log[i] = speed_low_pass_output
-    speed_m2_sqrt_log[i] = speed_m2_sqrt
-    speed_gradient_log[i] = speed_gradient
-    speed_setpoint_log[i] = speed_setpoint
-    sideslip_es_output_log[i] = current_sideslip
-    sideslip_high_pass_output_log[i] = sideslip_high_pass_output
-    sideslip_low_pass_output_log[i] = sideslip_low_pass_output
-    sideslip_m2_sqrt_log[i] = sideslip_m2_sqrt
-    sideslip_gradient_log[i] = sideslip_gradient
-    sideslip_setpoint_log[i] = sideslip_setpoint
-    
-    t += dt
-    current_speed = speed_es_output
-    current_sideslip = sideslip_es_output
-    
+    speed_es_output_log = np.zeros((step_num, 1))
+    speed_high_pass_output_log = np.zeros((step_num, 1))
+    speed_low_pass_output_log = np.zeros((step_num, 1))
+    speed_m2_sqrt_log = np.zeros((step_num, 1))
+    speed_gradient_log = np.zeros((step_num, 1))
+    speed_setpoint_log = np.zeros((step_num, 1))
 
-#==============================================================================
-# Make the plots
-#==============================================================================
-    
-fig, ax = plt.subplots(6, 1, sharex=True)
- 
-ax[0].plot(time_log, speed_setpoint_log, 'b--', label='setpoint')
-ax[0].plot(time_log, speed_es_output_log, 'b', label='output')
-ax[0].set_ylabel('speed [m/s]')
-ax[0].legend()
+    sideslip_es_output_log = np.zeros((step_num, 1))
+    sideslip_high_pass_output_log = np.zeros((step_num, 1))
+    sideslip_low_pass_output_log = np.zeros((step_num, 1))
+    sideslip_m2_sqrt_log = np.zeros((step_num, 1))
+    sideslip_gradient_log = np.zeros((step_num, 1))
+    sideslip_setpoint_log = np.zeros((step_num, 1))
 
-ax[1].plot(time_log, sideslip_setpoint_log*180/np.pi, 'r--', label='setpoint')
-ax[1].plot(time_log, sideslip_es_output_log*180/np.pi, 'r', label='output')
-ax[1].set_ylabel('sideslip [deg]')
-ax[1].legend()
+    t = 0
+    current_speed = init_speed
+    current_sideslip = init_sideslip
+    cost_init = cost_function(init_speed, init_sideslip)
+    for i in range(step_num):
+        cost = cost_function(current_speed, current_sideslip)
+        [
+            speed_es_output,
+            speed_high_pass_output,
+            speed_low_pass_output,
+            speed_m2_sqrt,
+            speed_gradient,
+            speed_setpoint,
+        ] = speed_extremum_seeking.update(cost - cost_init)
+        [
+            sideslip_es_output,
+            sideslip_high_pass_output,
+            sideslip_low_pass_output,
+            sideslip_m2_sqrt,
+            sideslip_gradient,
+            sideslip_setpoint,
+        ] = sideslip_extremum_seeking.update(cost - cost_init)
 
-ax[2].plot(time_log, cost_log)
-ax[2].set_ylabel('cost')
+        # log data
+        time_log[i] = t
+        cost_log[i] = cost
+        speed_es_output_log[i] = current_speed
+        speed_high_pass_output_log[i] = speed_high_pass_output
+        speed_low_pass_output_log[i] = speed_low_pass_output
+        speed_m2_sqrt_log[i] = speed_m2_sqrt
+        speed_gradient_log[i] = speed_gradient
+        speed_setpoint_log[i] = speed_setpoint
+        sideslip_es_output_log[i] = current_sideslip
+        sideslip_high_pass_output_log[i] = sideslip_high_pass_output
+        sideslip_low_pass_output_log[i] = sideslip_low_pass_output
+        sideslip_m2_sqrt_log[i] = sideslip_m2_sqrt
+        sideslip_gradient_log[i] = sideslip_gradient
+        sideslip_setpoint_log[i] = sideslip_setpoint
 
-ax[3].plot(time_log, speed_gradient_log, 'b', label='speed gradient')
-ax[3].plot(time_log, speed_m2_sqrt_log, 'b--', label = 'speed sqrt m2')
-ax[3].plot(time_log, sideslip_gradient_log, 'r', label='ssg radient')
-ax[3].plot(time_log, sideslip_m2_sqrt_log, 'r--', label = 'ss sqrt m2')
-ax[3].legend()
+        t += dt
+        current_speed = speed_es_output
+        current_sideslip = sideslip_es_output
 
-ax[4].plot(time_log, speed_high_pass_output_log, 'b', label='speed hp out')
-ax[4].plot(time_log, speed_low_pass_output_log, 'b--', label = 'speed lp out')
-ax[4].legend()
+    # Generate plots
 
-ax[5].plot(time_log, sideslip_high_pass_output_log, 'r', label='ss hp out')
-ax[5].plot(time_log, sideslip_low_pass_output_log, 'r--', label = 'ss lp out')
-ax[5].legend()
+    fig, ax = plt.subplots(6, 1, sharex=True)
 
-ax[-1].set_xlabel('time [s]')
+    ax[0].plot(time_log, speed_setpoint_log, "b--", label="setpoint")
+    ax[0].plot(time_log, speed_es_output_log, "b", label="output")
+    ax[0].set_ylabel("speed [m/s]")
+    ax[0].legend()
 
-plt.show()
+    ax[1].plot(time_log, sideslip_setpoint_log * 180 / np.pi, "r--", label="setpoint")
+    ax[1].plot(time_log, sideslip_es_output_log * 180 / np.pi, "r", label="output")
+    ax[1].set_ylabel("sideslip [deg]")
+    ax[1].legend()
 
-    
+    ax[2].plot(time_log, cost_log)
+    ax[2].set_ylabel("cost")
+
+    ax[3].plot(time_log, speed_gradient_log, "b", label="speed gradient")
+    ax[3].plot(time_log, speed_m2_sqrt_log, "b--", label="speed sqrt m2")
+    ax[3].plot(time_log, sideslip_gradient_log, "r", label="ssg radient")
+    ax[3].plot(time_log, sideslip_m2_sqrt_log, "r--", label="ss sqrt m2")
+    ax[3].legend()
+
+    ax[4].plot(time_log, speed_high_pass_output_log, "b", label="speed hp out")
+    ax[4].plot(time_log, speed_low_pass_output_log, "b--", label="speed lp out")
+    ax[4].legend()
+
+    ax[5].plot(time_log, sideslip_high_pass_output_log, "r", label="ss hp out")
+    ax[5].plot(time_log, sideslip_low_pass_output_log, "r--", label="ss lp out")
+    ax[5].legend()
+
+    ax[-1].set_xlabel("time [s]")
+    plt.show()
+
+
+if __name__ == "__main__":
+    run_simulation()
--- adaptive_multivariable_extreme_seeking_controller/extremum_seeking_control.py
+++ adaptive_multivariable_extreme_seeking_controller/extremum_seeking_control.py
@@ -1,36 +1,56 @@
 import numpy as np
-from first_order_filters import Low_pass_filter_first_order, High_pass_filter_first_order
+from first_order_filters import (
+    Low_pass_filter_first_order,
+    High_pass_filter_first_order,
+)
+
 
 class esc:
-    #cutoff_frequencies in rad/s
-    def __init__(self, sampling_period, disturbance_frequency, 
-                 disturbance_magnitude, integrator_gain, initial_search_value, 
-                 high_pass_cutoff_frequency, low_pass_cutoff_frequency, use_adapter):
-        self.dt = sampling_period #in [s]
-        self.disturbance_frequency = disturbance_frequency #in rad/s
+    # cutoff_frequencies in rad/s
+    def __init__(
+        self,
+        sampling_period,
+        disturbance_frequency,
+        disturbance_magnitude,
+        integrator_gain,
+        initial_search_value,
+        high_pass_cutoff_frequency,
+        low_pass_cutoff_frequency,
+        use_adapter,
+    ):
+        self.dt = sampling_period  # in [s]
+        self.disturbance_frequency = disturbance_frequency  # in rad/s
         self.disturbance_magnitude = disturbance_magnitude
-        self.integrator_gain = integrator_gain #negative for gradient descent
+        # negative for gradient descent
+        self.integrator_gain = integrator_gain
         self.initial_search_value = initial_search_value
-        self.use_adapter = use_adapter #boolean, true or false (use the adapter or not)
-        
-        self.high_pass_filter = High_pass_filter_first_order(sampling_period, high_pass_cutoff_frequency, 0, 0)
-        self.low_pass_filter = Low_pass_filter_first_order(sampling_period, low_pass_cutoff_frequency, 0)
-        
-        self.wt = 0 #current phase of perturbation
-        self.integral = 0 #integrator ouput
-        
-        self.m2 = 0 #estimated second moment
+        # boolean, true or false (use the adapter or not)
+        self.use_adapter = use_adapter
+
+        self.high_pass_filter = High_pass_filter_first_order(
+            sampling_period, high_pass_cutoff_frequency, 0, 0
+        )
+        self.low_pass_filter = Low_pass_filter_first_order(
+            sampling_period, low_pass_cutoff_frequency, 0
+        )
+        # current phase of perturbation
+        self.wt = 0
+        # integrator output
+        self.integral = 0
+        # estimated second moment
+        self.m2 = 0
         self.b2 = 0.9
-        self.epsilon = 1e-8 #to prevent from dividing by zero
-        
+        # to prevent from dividing by zero
+        self.epsilon = 1e-8
+
         return
-    
+
     def update(self, cost):
         high_pass_output = self.high_pass_filter.apply(cost)
         low_pass_input = high_pass_output * np.sin(self.wt)
         low_pass_output = self.low_pass_filter.apply(low_pass_input)
         gradient = 0
-        
+
         if self.use_adapter:
             self.m2 = self.b2 * self.m2 + (1 - self.b2) * np.power(low_pass_output, 2)
             if np.sqrt(self.m2) > 1:
@@ -40,16 +60,22 @@ class esc:
 
         else:
             gradient = low_pass_output
-            
-        self.integral +=  self.integrator_gain * gradient * self.dt
+
+        self.integral += self.integrator_gain * gradient * self.dt
         setpoint = self.initial_search_value + self.integral
         output = self.disturbance_magnitude * np.sin(self.wt) + setpoint
         perturbation = self.disturbance_magnitude * np.sin(self.wt)
 
-        #update wt
+        # update wt
         self.wt += self.disturbance_frequency * self.dt
         if self.wt > 2 * np.pi:
             self.wt -= 2 * np.pi
-            
-        return output, high_pass_output, low_pass_output, (np.sqrt(self.m2) + self.epsilon), gradient, setpoint
-    
+
+        return (
+            output,
+            high_pass_output,
+            low_pass_output,
+            (np.sqrt(self.m2) + self.epsilon),
+            gradient,
+            setpoint,
+        )
